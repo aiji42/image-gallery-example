8b738c7fc102a49659587cb9bd2d7f8bc98f87d6
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-10-20 21:00:00 +0900

    add blur page

---
+++ src/app/blur/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-8 text-slate-400">Image Gallery</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {Array.from({ length: 100 }).map((_, index) => (
+          <div
+            className="aspect-w-16 aspect-h-9 overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow"
+            key={index}
+          >
+            <img
+              src={`https://img-optmize.aiji42.com?src=https://picsum.photos/seed/${
+                index * 10
+              }/1200/1800`}
+              alt="Image 1"
+              className="w-full h-full object-cover"
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
