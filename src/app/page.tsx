5f90550873687ec934dd240a376fdc75d2c9e267
Aiji Uejima <aiji-uejima@cybozu.co.jp>
2023-10-30 21:00:00 +0900

    update top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const pad = Number(searchParams.pad);
 
-export default function Home() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-8 text-slate-400">Image Gallery</h1>
@@ -11,7 +15,9 @@ export default function Home() {
             key={index}
           >
             <img
-              src={`https://picsum.photos/seed/${index * 10}/2400/1600`}
+              src={`https://picsum.photos/seed/${
+                index * 10 + (Number.isSafeInteger(pad) ? pad : 0)
+              }/2400/1600`}
               alt="Image 1"
               className="w-full h-full object-cover"
               loading="lazy"
